35099f09510551599123e50b4a147e979b0b9da0
ANGIELOUX <angelajohnson943@gmail.com>
2022-05-25 17:48:02 +1000

    Define latitude state

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Clock App
 ## *Learning Class Components, State & Lifecycle in React*
 
-### What is a component? 
+## What is a component? 
 It is the smallest unit of a web page which can be configured, reused and rested. It can be thought of as a function that returns HTML elements. 
 
 ## How to design a component
@@ -14,3 +14,11 @@ A single tweet is also a component, which incporates the three core features of 
 - Configurable: can alter the image, content, etc.
 - Nested: has various child components-- comments/chat, retweet, like, etc.
 - Reusable: is structured so that it can be reused in multiple places
+
+
+## What is State? 
+Each time a State value is updated, it triggers the page to re-render. 
+React is smart--and so the entire page does not refresh, however only the part of the component that is changed re-renders. (e.g. the seconds hand of the Clock)
+
+The React.js [docs]('https://reactjs.org/docs/state-and-lifecycle.html') do a great job of explaining this. 
+
--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,61 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Clock from './Clock';
 
-const App = () => {
-  return (
-    <Clock date={new Date()} />
-    
-  )
+
+class App extends Component {
+  // constructor method runs
+  constructor(props) {
+    super(props)
+    // define a state
+    this.state = {latitude: null, errorMessage: ''}
+
+    // api call is made asynchronously & latitude value is null
+    // once it is completed, the latitude value changes
+    // because the state has now changed, the page will re-render with the new latitude value
+    window.navigator.geolocation.getCurrentPosition(
+      position => this.setState({latitude: position.coords.latitude}),
+      error => this.setState({errorMessage: error.message})
+      )
+
+  }
+  render(){
+    const {latitude, errorMessage} = this.state
+    return (
+    <div>
+      <h1>{latitude}</h1>
+        {errorMessage || <Clock date={new Date()}/>}
+    </div>
+    )
+  }
 }
 
 export default App;
+
+
+// 1. The App component is created
+// 2. Make an API call to retrieve the location. 
+// 3. Since this is async, the code continues and the HTML element/s is/are returned--> at this point in time, the position value is null(because it takes time for the API call). This is why you would have to set a state (done above).
+
+
+// const App = () => {
+//   // let position = null 
+//   // // make API call
+//   // // API calls are asynchronous
+//   //   window.navigator.geolocation.getCurrentPosition(
+//   //     position => console.log(position),
+//   //     error => console.log(error)
+//   //   )
+
+//   // // return the HTML element
+//   // return (
+//   //   <div>
+//   //     <h1>{position}</h1>
+//   //     <Clock date={new Date()} />
+//   //   </div>
+    
+    
+    
+//   // )
+// }
+
+
